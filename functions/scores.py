c31209931d3b93a6f8bd1ac4ec56e87fce6c17a9
tez4 <joel.twann@bluewin.ch>
2023-11-23 19:12:17 +0100

    Added score function

---
+++ functions/scores.py
@@ -0,0 +1,14 @@
+import io
+import torch
+import numpy as np
+import scipy.ndimage as ndi
+
+
+def mean_pixel_distance(outputs, targets):
+    assert outputs.shape == targets.shape, "output and target shapes must be equal"
+    assert len(outputs.shape) == 4, "outputs and targets must be 4D tensors (batch, channels, height, width)"
+
+    differences = (outputs - targets) ** 2
+    mse_per_image = differences.mean(dim=[1, 2, 3])
+
+    return mse_per_image
